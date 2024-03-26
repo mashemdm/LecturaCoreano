9ec9dada8215c1ac2cf829230969835d8dd5a686
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 12:30:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
 
-"""if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-"""    
-
 if uploaded_file is not None:
     # To read image file buffer with OpenCV:
     img_bytes = uploaded_file.read()
@@ -32,6 +26,6 @@ if uploaded_file is not None:
          cv2_img=cv2.bitwise_not(cv2_img)
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
+    text=pytesseract.image_to_string(img_rgb, lang="Kor")
     st.write(text) 
     
