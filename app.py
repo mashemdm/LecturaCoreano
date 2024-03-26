7693218b2b0108c02573b45d25717e725b330871
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 11:32:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ if uploaded_file is not None:
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
        # Ensure Tesseract is configured for Korean recognition
-      pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Replace with your Tesseract path (adjust accordingly)
     config = '-l kor'  # Specify Korean language code
       
     text=pytesseract.image_to_string(img_rgb, config=config)
