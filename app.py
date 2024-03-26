73171eea0174f1e528fd47cd0e4b8fb190a8a4e4
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 12:45:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,6 @@ if uploaded_file is not None:
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     # text=pytesseract.image_to_string(img_rgb)
-    text = pytesseract.image_to_string(img_rgb, lang='kor', config=tessdata_dir_config)  
+    text = pytesseract.image_to_string(img_rgb, lang='kor')  
     st.write(text) 
     
