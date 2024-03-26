8159bd0dbc9616d51f63e8cbd521360ba4ea36b6
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 12:19:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import cv2
 import numpy as np
@@ -15,13 +14,6 @@ uploaded_file = st.file_uploader("Sube una imagen", type=["jpg", "jpeg", "png"])
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
-
-"""if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-"""    
-
 if uploaded_file is not None:
     # To read image file buffer with OpenCV:
     img_bytes = uploaded_file.read()
@@ -31,12 +23,13 @@ if uploaded_file is not None:
       
     if filtro == 'Con Filtro':
          cv2_img=cv2.bitwise_not(cv2_img)
-    """else:
-         cv2_img= cv2_img"""
+    else:
+         cv2_img= cv2_img
     
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
+      
+    text=pytesseract.image_to_string(img_rgb,)
     st.write(text) 
     
 
