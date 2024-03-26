90218f31d681f1a072428c49225f7fba13690a97
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 11:46:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,12 @@ import cv2
 import numpy as np
 import pytesseract
 from PIL import Image
+
 st.title("Reconocimiento óptico de Caracteres")
 # img_file_buffer = st.camera_input("Toma una Foto")
 uploaded_file = st.file_uploader("Sube una imagen", type=["jpg", "jpeg", "png"])
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
-
-"""if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-"""    
 
 if uploaded_file is not None:
     # To read image file buffer with OpenCV:
@@ -24,8 +18,6 @@ if uploaded_file is not None:
       
     if filtro == 'Con Filtro':
          cv2_img=cv2.bitwise_not(cv2_img)
-    """else:
-         cv2_img= cv2_img"""
     
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
