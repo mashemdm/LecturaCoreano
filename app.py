bc678aacf8e0514a4555baae92a7c3364f01ac43
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-03-26 12:45:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ import pytesseract
 from PIL import Image
 
 
+
+
+
+pytesseract.pytesseract.tesseract_cmd  = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
+
 st.title("Reconocimiento óptico de Caracteres")
+
 
 # img_file_buffer = st.camera_input("Toma una Foto")
 uploaded_file = st.file_uploader("Sube una imagen", type=["jpg", "jpeg", "png"])
-
 
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
@@ -26,6 +33,7 @@ if uploaded_file is not None:
          cv2_img=cv2.bitwise_not(cv2_img)
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
+    # text=pytesseract.image_to_string(img_rgb)
+    text = pytesseract.image_to_string(img_rgb, lang='kor', config=tessdata_dir_config)  
     st.write(text) 
     
